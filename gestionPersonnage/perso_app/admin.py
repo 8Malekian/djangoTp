620629c87c0357005e4903caf6f95c6c5d863596
Dijkstra <contiemathias@gmail.com>
2020-01-25 23:44:55 +0100

    feat: update de la bd et compte admin

--- gestionPersonnage/perso_app/admin.py
+++ gestionPersonnage/perso_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Personnage
 
 # Register your models here.
+class PersonnageGestion(admin.ModelAdmin):
+    list_display = ("pk","nom","classe","force","dexterite","constition","intelligence",
+    "sagesse","charisme","poids","user")
+
+admin.site.register(Personnage,PersonnageGestion)
