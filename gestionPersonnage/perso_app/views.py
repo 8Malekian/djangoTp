baa2f935e03d2942a357d04dfceb6b184b85efb9
Dijkstra <contiemathias@gmail.com>
2020-01-25 21:54:11 +0100

    feat: page home html

--- gestionPersonnage/perso_app/views.py
+++ gestionPersonnage/perso_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+
+    template = loader.get_template("gestionPersonnage/home.html")
+    context = {
+        "homeText": "Accueil",
+    }
+    return HttpResponse(template.render(context, request))
