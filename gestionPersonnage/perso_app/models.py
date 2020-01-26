046b866da16b8663fbac2975a3f293ecea5cede8
Dijkstra <contiemathias@gmail.com>
2020-01-26 11:42:12 +0100

    bugfix: lien sur la page home

--- gestionPersonnage/perso_app/models.py
+++ gestionPersonnage/perso_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
+
 User = get_user_model()
 # Create your models here.
 
